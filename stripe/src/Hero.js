3da6c56125a3824c43551a2d7069b2b5e355d514
tonyclem <tonyclem001@gmail.com>
2022-06-24 22:05:38 +0200

    first commit of stripe

---
+++ stripe/src/Hero.js
@@ -0,0 +1,28 @@
+import React from "react";
+import phoneImg from "./images/phone.svg";
+import { useGlobalContext } from "./context";
+
+const Hero = () => {
+  const { closeSubmenu } = useGlobalContext();
+
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure of the internet </h1>
+          <p>
+            Millions of companies of all sizes-form startups to Fortune 500s-use
+            Stripe's software and APIs to accept Payments, send payouts, and
+            manage their business online.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ stripe/src/context.js
@@ -0,0 +1,44 @@
+import React, { useState, useContext } from "react";
+// import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openSubmenu = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSubmenuOpen,
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        openSubmenu,
+        closeSubmenu,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
