2b9c72506717ab1445c1039979dbb3f00c817338
tonyclem <tonyclem001@gmail.com>
2022-06-27 15:08:15 +0200

    added fetching data method, and increasing the basket

--- cart-app/src/reducer.js
+++ cart-app/src/reducer.js
@@ -7,6 +7,7 @@ const reducer = (state, action) => {
         ...state,
         cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
       };
+
     case "INCREASE":
       let tempCart = state.cart.map((cartItem) => {
         if (cartItem.id === action.payload) {
@@ -15,6 +16,7 @@ const reducer = (state, action) => {
         return cartItem;
       });
       return { ...state, cart: tempCart };
+
     case "DECREASE":
       let tempCartDecr = state.cart
         .map((cartItem) => {
@@ -25,6 +27,28 @@ const reducer = (state, action) => {
         })
         .filter((cartItem) => cartItem.amount !== 0);
       return { ...state, cart: tempCartDecr };
+
+    case "GET_TOTALS":
+      let { total, amount } = state.cart.reduce(
+        (cartTotal, cartItem) => {
+          const { price, amount } = cartItem;
+          const itemTotal = price * amount;
+
+          cartTotal.total += itemTotal;
+          cartTotal.amount += amount;
+          return cartTotal;
+        },
+        { total: 0, amount: 0 }
+      );
+      total = parseFloat(total.toFixed(2));
+      return { ...state, total, amount };
+
+    case "LOADING":
+      return { ...state, loading: true };
+
+    case "DISPLAY_ITEMS":
+      return { ...state, cart: action.payload, loading: false };
+
     default:
       return state;
   }
