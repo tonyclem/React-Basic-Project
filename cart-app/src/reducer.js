aed3a2741decc8c23ce555dd290ff48fa9b0e579
tonyclem <tonyclem001@gmail.com>
2022-06-27 13:19:36 +0200

    add increasing function

--- cart-app/src/reducer.js
+++ cart-app/src/reducer.js
@@ -7,19 +7,38 @@ const reducer = (state, action) => {
         ...state,
         cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
       };
+    case "INCREASE":
+      let tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount + 1 };
+        }
+        return cartItem;
+      });
+      return { ...state, cart: tempCart };
     default:
       return state;
   }
-  //   if (action.type === "CLEAR_CART") {
-  //     return { ...state, cart: [] };
-  //   }
-  //   if(action.type === "REMOVE"){
-  //     return {
-  //     ...state,
-  //     cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
-  //   };
-  //   }
-  //   return state;
 };
 
 export default reducer;
+
+// CONDITIONAL FUNCTIONS
+//   if (action.type === "CLEAR_CART") {
+//     return { ...state, cart: [] };
+//   }
+//   if(action.type === "REMOVE"){
+//     return {
+//     ...state,
+//     cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+//   };
+//   }
+//   if(action.payload === "INCREASE"){
+//     let tempCart = state.cart.map((cartItem) => {
+//         if (cartItem.id === action.payload) {
+//           return { ...cartItem, amount: cartItem.amount + 1 };
+//         }
+//         return cartItem
+//       });
+//       return { ...state, cart: tempCart };
+//   }
+//   return state;
