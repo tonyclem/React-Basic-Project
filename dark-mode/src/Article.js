543f44e159375c1cb5066d208a6ef43d1683ee9c
tonyclem <tonyclem001@gmail.com>
2022-07-02 00:02:32 +0200

    added light and dark mode app

---
+++ dark-mode/src/Article.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import moment from 'moment'
+
+const Article = ({title, snippet, data, length}) => {
+  return (<article className="post">
+      <h2>{title}</h2>
+      <div className="post-info">
+        <span>{moment(data).format('dddd Do, YYYY')}</span>
+        <span>{length} min read</span>
+        <p>{snippet}</p>
+      </div>
+     </article>
+  )
+}
+
+export default Article
