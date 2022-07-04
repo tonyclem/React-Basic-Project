8460215c325c26d2d7d48b04853ae14d74bff408
tonyclem <tonyclem001@gmail.com>
2022-07-04 21:37:29 +0200

    added get single movie info

--- movie-db/src/SingleMovie.js
+++ movie-db/src/SingleMovie.js
@@ -12,12 +12,49 @@ const SingleMovie = () => {
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
+    if (data.Response === "False") {
+      setError({ show: true, msg: data.Error });
+      setIsLoading(false);
+    } else {
+      setMovie(data);
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
     fetchMovie(`${API_ENDPOINT}&i=${id}`);
   }, [id]);
-  return <h2>single movie</h2>;
+
+  if (isLoading) {
+    return <div className="loading"></div>;
+  }
+
+  if (error.show) {
+    return (
+      <div className="page-error">
+        <h1>{error.msg}</h1>
+        <Link to="/" className="btn">
+          back to movies
+        </Link>
+      </div>
+    );
+  }
+
+  const { Poster: poster, Title: title, Plot: plot, Year: year } = movie;
+
+  return (
+    <section className="single-movie">
+      <img src={poster} alt={title} />
+      <div className="single-movie-info">
+        <h2>{title}</h2>
+        <p>{plot}</p>
+        <h4>{year}</h4>
+        <Link to="/" className="btn">
+          back to movies
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default SingleMovie;
