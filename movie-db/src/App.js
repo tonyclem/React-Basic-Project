c5d9543ab5620f75b3770d601c7335c389bac140
tonyclem <tonyclem001@gmail.com>
2022-07-04 20:39:33 +0200

    initial commit

---
+++ movie-db/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./Home";
+import Movie from "./SingleMovie";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/movies/:id" element={<Movie />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ movie-db/src/SingleMovie.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { API_ENDPOINT } from "./context";
+
+const SingleMovie = () => {
+  const { id } = useParams();
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  const fetchMovie = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchMovie(`${API_ENDPOINT}&i=${id}`);
+  }, [id]);
+  return <h2>single movie</h2>;
+};
+
+export default SingleMovie;
