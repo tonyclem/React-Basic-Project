c667b9bcab9743cce36411c76c9dd14fdba76109
tonyclem <tonyclem001@gmail.com>
2022-07-05 11:53:25 +0200

    added show modal

--- quiz-game/src/context.js
+++ quiz-game/src/context.js
@@ -49,6 +49,7 @@ const AppProvider = ({ children }) => {
     setIndex((oldIndex) => {
       const index = oldIndex + 1;
       if (index > questions.length - 1) {
+        openModal();
         return 0;
       } else {
         return index;
@@ -62,6 +63,16 @@ const AppProvider = ({ children }) => {
       setCorrect((oldState) => oldState + 1);
     }
     nextQuestion();
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setWaiting(true);
+    setCorrect(0);
+    setIsModalOpen(false);
   };
 
   useEffect(() => {
@@ -80,6 +91,7 @@ const AppProvider = ({ children }) => {
         error,
         nextQuestion,
         checkAnswer,
+        closeModal,
       }}
     >
       {children}
