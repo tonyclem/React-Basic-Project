862caca07a667742b1d79d1c120e6a25ac473216
tonyclem <tonyclem001@gmail.com>
2022-07-05 10:09:32 +0200

    context setup

---
+++ quiz-game/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useGlobalContext } from "./context";
+
+import SetupForm from "./SetupForm";
+import Loading from "./Loading";
+import Modal from "./Modal";
+function App() {
+  const { waiting, loading, questions, index, correct } = useGlobalContext();
+  if (waiting) {
+    return <SetupForm />;
+  }
+  if (loading) {
+    return <Loading />;
+  }
+  return <main>app quiz</main>;
+}
+
+export default App;
