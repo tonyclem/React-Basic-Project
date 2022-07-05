85b28012e83b70072e06d0c4262203d6cffd41e4
tonyclem <tonyclem001@gmail.com>
2022-07-05 07:23:51 +0200

    added search form and functionality

--- hacker-news/src/context.js
+++ hacker-news/src/context.js
@@ -39,13 +39,23 @@ const AppProvider = ({ children }) => {
     }
   };
 
+  const removeStory = (id) => {
+    dispatch({ type: REMOVE_STORY, payload: id });
+  };
+
+  const handleSearch = (query) => {
+    dispatch({ type: HANDLE_SEARCH, payload: query });
+  };
+
   useEffect(() => {
     fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
     // eslint-disable-next-line
-  }, []);
+  }, [state.query]);
 
   return (
-    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state, removeStory, handleSearch }}>
+      {children}
+    </AppContext.Provider>
   );
 };
 
